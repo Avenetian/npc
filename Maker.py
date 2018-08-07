919592932f47deccc2d82a63d38779dc041320fb
Miklos Bognar <bgrm92@gmail.com>
2018-08-07 16:48:33 +0200

    Initial Commit

---
+++ Maker.py
@@ -0,0 +1,68 @@
+#! /usr/bin/python3
+
+print ('Hello World!')
+import json
+from tkinter import *
+window = Tk()
+
+
+window.title('TestWindow')
+window.configure(background = 'IndianRed4')
+Label(window, text='Filename', bg='IndianRed4', fg = 'black', font='none 12 bold').grid(row=0, column=0)
+Label(window, text='Név', bg='IndianRed4', fg = 'black', font='none 12 bold').grid(row=1, column=0)
+Label(window, text='Té', bg='IndianRed4', fg = 'black', font='none 12 bold').grid(row=2, column=0)
+Label(window, text='Ké', bg='IndianRed4', fg = 'black', font='none 12 bold').grid(row=3, column=0)
+Label(window, text='Vé', bg='IndianRed4', fg = 'black', font='none 12 bold').grid(row=4, column=0)
+Label(window, text='Ép', bg='IndianRed4', fg = 'black', font='none 12 bold').grid(row=5, column=0)
+Label(window, text='Fp', bg='IndianRed4', fg = 'black', font='none 12 bold').grid(row=6, column=0)
+Label(window, text='Sp', bg='IndianRed4', fg = 'black', font='none 12 bold').grid(row=7, column=0)
+Label(window, text='Képzettségek', bg='IndianRed4', fg = 'black', font='none 12 bold').grid(row=8, column=0)
+Label(window, text='Ellenállások', bg='IndianRed4', fg = 'black', font='none 12 bold').grid(row=9, column=0)
+Label(window, text='Tulajdonságok', bg='IndianRed4', fg = 'black', font='none 12 bold').grid(row=10, column=0)
+e0=Entry(window)
+e0.grid(row=0,column=1)
+e1=Entry(window)
+e1.grid(row=1,column=1)
+e2=Entry(window)
+e2.grid(row=2,column=1)
+e3=Entry(window)
+e3.grid(row=3,column=1)
+e4=Entry(window)
+e4.grid(row=4,column=1)
+e5=Entry(window)
+e5.grid(row=5,column=1)
+e6=Entry(window)
+e6.grid(row=6,column=1)
+
+e7=Entry(window)
+e7.grid(row=7,column=1)
+
+e8=Entry(window)
+e8.grid(row=8,column=1)
+
+e9=Entry(window)
+e9.grid(row=9,column=1)
+e10=Entry(window)
+e10.grid(row=10,column=1)
+
+def save():
+	filename = e0.get()
+	data1  = e1.get()
+	data2 = e2.get()
+	data3 = e3.get()
+	data4 = e4.get()
+	data5 = e5.get()
+	data6 = e6.get()
+	data7=e7.get()
+	data8=e8.get()
+	data9=e9.get()
+
+	data10=e10.get()
+	data = {'Nev':data1, 'Te': data2, 'Ke':data3, 'Ve':data4, 'Ep':data5, 'Fp':data6, 'Sp':data7, 'Kepz':data8, 'Ell':data9, 'Tul':data10}
+	with open (filename + '.json' , 'w') as output:
+		json.dump(data, output)
+
+b = Button (window, text="Save", width=10, command=save)
+b.grid(row=11,column=0)
+window.mainloop()
+
---
+++ Reader.py
@@ -0,0 +1,104 @@
+#! /usr/bin/python3
+import sys
+import json
+from tkinter import *
+readw = Tk()
+serial = 0
+readw.title('ReaderMenu')
+Label(readw, text= "Read What?", bg='Gray40', fg = "white", font='none 12 bold').grid(row=0,column=0)
+e0=Entry(readw)
+e0.grid(row=0,column=1)
+readw.configure(background = 'Gray40')
+
+def opener():
+	#elcsiValue=int(e4.get())-4
+
+	filename = e0.get()
+	with open (filename + '.json','r') as f:
+		data=json.load(f)
+	
+	opened = Tk()
+	opened.title(filename+(str(serial)))
+	opened.configure(background = 'Gray40')
+
+
+	Label(opened, text="NÉV: ",bg='Gray40', fg = 'white', font='none 12 bold').grid(row=0,column=0)
+	e1=Entry(opened,bg='Gray50')
+	e1.insert(0,data["Nev"])
+	e1.grid(row=0,column=1)
+
+
+	Label(opened, text="TÉ: ",bg='Gray40', fg = 'white', font='none 12 bold').grid(row=1,column=0)
+	e2=Entry(opened, bg='Gray50')
+	e2.insert(0,data["Te"])
+	e2.grid(row=1,column=1)
+
+
+	Label(opened, text="KÉ: ",bg='Gray40', fg = 'white', font='none 12 bold').grid(row=2,column=0)
+	e3=Entry(opened,bg='Gray50')
+	e3.insert(0,data["Ke"])
+	e3.grid(row=2,column=1)
+
+	Label(opened, text="VÉ: ",bg='Gray40', fg = 'white', font='none 12 bold').grid(row=3,column=0)
+	e4=Entry(opened,bg='Gray50')
+	e4.insert(0,data["Ve"])
+	e4.grid(row=3,column=1)
+
+	Label(opened, text="ÉP: ",bg='Gray40', fg = 'white', font='none 12 bold').grid(row=4,column=0)
+	e5=Entry(opened,bg='Gray50')
+	e5.insert(0,data["Ep"])
+	e5.grid(row=4,column=1)
+
+
+	Label(opened, text="FP: ",bg='Gray40', fg = 'white', font='none 12 bold').grid(row=5,column=0)
+	e6=Entry(opened,bg='Gray50')
+	e6.insert(0,data["Fp"])
+	e6.grid(row=5,column=1)
+
+	Label(opened, text="SP: ",bg='Gray40', fg = 'white', font='none 12 bold').grid(row=6,column=0)
+	e7=Entry(opened,bg='Gray50')
+	e7.insert(0,data["Sp"])
+	e7.grid(row=6,column=1)
+
+	Label(opened, text="Képzettségek: ",bg='Gray40', fg = 'white', font='none 12 bold').grid(row=7,column=0)
+	Label(opened, text=data["Kepz"],bg='Gray40', fg = 'white', font='none 12 bold').grid(row=7,column=1)
+	
+	Label(opened, text="Ellenállások: ",bg='Gray40', fg = 'white', font='none 12 bold').grid(row=8,column=0)
+	Label(opened, text=data["Ell"],bg='Gray40', fg = 'white', font='none 12 bold').grid(row=8,column=1)
+	
+	Label(opened, text="Tulajdonságok: ",bg='Gray40', fg = 'white', font='none 12 bold').grid(row=9,column=0)
+	Label(opened, text=data["Tul"],bg='Gray40', fg = 'white', font='none 12 bold').grid(row=9,column=1)
+	
+	def elcsi():
+		newVe=int(e4.get())
+		newVe=newVe-4
+		e4.delete(0,END)
+		e4.insert(0,newVe)
+		e4.configure(bg='red')
+
+	def elcsiOff():
+			pass
+
+
+	b1 = Checkbutton(opened,text='Elcsigázott',bg='green', width=10, command=elcsi).grid(row=10,column=0)
+	
+
+	opened.mainloop()
+def plusser():
+	pass
+
+
+def combine():
+	global serial
+	serial=serial+1
+	opener()
+def elcsi():
+	newVe=int(e4.get())
+	newVe=newVe-4
+	e4.insert(0,newVe)
+def exit():
+	sys.exit(0)
+b = Button (readw,text='Open', width=10, command=combine).grid(row=1,column=0)
+b_quit = Button (readw, text='Quit All', width= 20, command=exit).grid(row=1,column=1)
+
+readw.mainloop()
