2cbc59a26b4126e7505f4d2588214bcfc472c504
Miklos Bognar <bgrm92@gmail.com>
2018-08-09 16:08:28 +0200

    Labels and Entries in individual classes, working printing method

--- rerun.py
+++ rerun.py
@@ -1,46 +1,41 @@
 #! /usr/bin/python3
 from tkinter import *
 import json
-print ('Hello World')
-
-class Window(object):
+names=['Filename', 'Name', 'Té','Ké','Vé','Ép','Fp','Képzettségek','Ellenállások','Tulajdonságok']
+dicta=[]
+dicta1=[]
+class Window:
 	def __init__(self):
 		self.app=Tk()
 		self.app.title('Test Window')
-		
-	def label(self,szoveg,row,col):
-		self.szoveg=szoveg
-		self.row=row
-		self.col=col
-		Label(self.app, text=szoveg,bg='IndianRed4', fg = 'black', font='none 12 bold').grid(row=row, column=col)
-	def ent(self,row,col):
-		self.entry=Entry(self.app)
-		self.entry.grid(row=row,column=col+1)
-		
-		
-	#def printer(self):
-	#	self.data=self.entry.get()
-	#	print (self.data)
-		
-
-	#def button(self,row,col):
-	#	Button(self.app,text='Butt',command=self.printer).grid(row=row+1,column=col)
 	
 	def runner(self):
 		self.app.mainloop()
 
+class Lab:
+	def __init__(self,szoveg,row,col):
+		Label(root.app, text=szoveg,bg='IndianRed4', fg = 'black', font='none 12 bold').grid(row=row, column=col)
+class Ent:
+	def __init__(self,row,col):
+		self.entry=Entry(root.app)
+		self.entry.grid(row=row,column=col+1)
+		self.data=23
 
+def maker():
 	
+	for i in range(len(names)):
+		dicta.append(Lab(names[i],i,0))
+		b.grid(row=1+i,column=0)
+		b1.grid(row=1+i,column=1)
+		dicta1.append(Ent(i,0))
 root=Window()
-root.label('Hello',0,0)
-root.label('Darkness',1,0)
-e1=root.ent(0,0)
-e2=root.ent(1,0)
+
 
 def printer():
-	data=root.entry.get()
-	print (data)
-b=Button(root.app,text='Butt',command=printer)
-b.grid(row=2,column=0)
-
+	for i in range(len(dicta1)):
+		print (dicta1[i].entry.get())
+b=Button(root.app,text='Maker',command=maker)
+b.grid(row=0,column=0)
+b1=Button(root.app, text='Printer',command=printer)
+b1.grid(row=0,column=1)
 root.runner()
